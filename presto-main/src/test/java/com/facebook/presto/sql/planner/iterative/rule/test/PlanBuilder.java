04172df25bc2435bfb6552337f1206dd3429975a
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2017-02-20 16:49:38 -0500

    Migrate PushTableWriteThroughUnion
    Migrate PushTableWriteThroughUnion to the iterative optimizer.

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
@@ -26,6 +26,7 @@ import com.facebook.presto.sql.planner.PartitioningScheme;
 import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.TestingTableHandle;
+import com.facebook.presto.sql.planner.TestingWriterTarget;
 import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.Assignments;
 import com.facebook.presto.sql.planner.plan.DeleteNode;
@@ -41,7 +42,6 @@ import com.facebook.presto.sql.planner.plan.TableWriterNode;
 import com.facebook.presto.sql.planner.plan.UnionNode;
 import com.facebook.presto.sql.planner.plan.ValuesNode;
 import com.facebook.presto.sql.tree.Expression;
-import com.facebook.presto.util.ImmutableCollectors;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ListMultimap;
 
@@ -54,8 +54,11 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import static com.facebook.presto.spi.type.BigintType.BIGINT;
+import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;
 import static com.facebook.presto.sql.planner.SystemPartitioningHandle.FIXED_HASH_DISTRIBUTION;
 import static com.facebook.presto.sql.planner.SystemPartitioningHandle.SINGLE_DISTRIBUTION;
+import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static java.lang.String.format;
 
 public class PlanBuilder
@@ -216,24 +219,22 @@ public class PlanBuilder
 
     public TableWriterNode tableWriter(
             PlanNode source,
-            TableWriterNode.WriterTarget target,
             List<Symbol> columns,
-            List<String> columnNames,
-            List<Symbol> outputs,
-            Optional<PartitioningScheme> partitioningScheme)
+            List<String> columnNames)
     {
         return new TableWriterNode(
                 idAllocator.getNextId(),
                 source,
-                target,
+                new TestingWriterTarget(),
                 columns,
                 columnNames,
-                outputs,
-                partitioningScheme);
+                ImmutableList.of(symbol("partialrows", BIGINT), symbol("fragment", VARBINARY)),
+                Optional.empty());
     }
 
-    public UnionNode union(List<PlanNode> sources, ListMultimap<Symbol, Symbol> outputsToInputs, List<Symbol> outputs)
+    public UnionNode union(List<PlanNode> sources, ListMultimap<Symbol, Symbol> outputsToInputs)
     {
+        ImmutableList<Symbol> outputs = outputsToInputs.keySet().stream().collect(toImmutableList());
         return new UnionNode(idAllocator.getNextId(), sources, outputsToInputs, outputs);
     }
 
@@ -262,7 +263,7 @@ public class PlanBuilder
     {
         return Stream.of(expressions)
                 .map(PlanBuilder::expression)
-                .collect(ImmutableCollectors.toImmutableList());
+                .collect(toImmutableList());
     }
 
     public Map<Symbol, Type> getSymbols()
